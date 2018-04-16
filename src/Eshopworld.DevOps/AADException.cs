2f4237a575ce7efacc32beab6446d84884a4139a
Vaclav Haken <vaclav@vaclavkabat.net>
2018-04-16 13:37:29 +0100

    code smell fix

--- src/Eshopworld.DevOps/AADException.cs
+++ src/Eshopworld.DevOps/AADException.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Eshopworld.DevOps
 {
     /// <summary>
     /// AAD specific exception
     /// </summary>
+    [Serializable]
     public class AADException :Exception
     {
         /// <summary>
@@ -13,6 +15,10 @@ namespace Eshopworld.DevOps
         /// <param name="msg">exception message</param>
         public AADException(string msg):base(msg)
         {            
+        }
+
+        protected AADException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
         }
     }
 }
