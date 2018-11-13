1cec50b5f5c78947a94b7f821be6fb71dc00e625
Vaclav Haken <vaclav@vaclavkabat.net>
2018-11-13 13:45:22 +0000

    added the enum, version bump

---
+++ src/Eshopworld.DevOps/TenantSizeEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace Eshopworld.DevOps
+{
+    /// <summary>
+    /// tenant sizing, each tenant will be classified as one of these recognized sizes
+    /// </summary>
+    public enum TenantSizeEnum
+    {
+        Small,
+        Large,
+        Enterprise
+    }
+}
