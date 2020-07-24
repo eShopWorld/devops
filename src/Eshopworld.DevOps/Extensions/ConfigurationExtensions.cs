f1c06ae789e265c98c0bf0d3e3ed984d586193f4
Robert McCabe <rmccabe@eshopworld.com>
2020-07-24 08:35:48 +0100

    Added extension methods for adding individual key vault secrets and other useful configuration extensions

---
+++ src/Eshopworld.DevOps/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,227 @@
+﻿namespace Microsoft.Extensions.Configuration
+{
+    using Azure.KeyVault.Models;
+    using Azure.KeyVault;
+    using Azure.Services.AppAuthentication;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+
+    /// <summary>Class Configuration extensions.</summary>
+    public static class ConfigurationExtensions
+    {
+        /// <summary>
+        /// Binds the base section of the config to an actual class of type T.
+        /// </summary>
+        /// <param name="config">The configuration.</param>
+        /// <returns>IConfigurationSection.</returns>
+        public static T BindBaseSection<T>(this IConfiguration config)
+        {
+            if (config == null)
+                throw new ArgumentException("Configuration must be set", nameof(config));
+
+            var configBase = new ConfigurationBuilder();
+            configBase.AddInMemoryCollection(config.GetChildren().Where(c => c.Value != null).Select(c => new KeyValuePair<string, string>("base:" + c.Key, c.Value)));
+            return configBase.Build().GetSection("base").Get<T>();
+        }
+
+        /// <summary>
+        /// Adds the kubernetes secrets config.  Reads from a directory where it takes the file name as the key (config property) and the
+        /// value is the content within the file.
+        /// </summary>
+        /// <param name="builder">The configuration builder to bind to.</param>
+        /// <param name="path">The path to the directory containing the secrets (defaults to "secrets").</param>
+        /// <param name="optional">if set to <c>true</c>, ignore if does not exist [optional].</param>
+        /// <returns>The configuration builder after config has been added.</returns>
+        public static IConfigurationBuilder AddKubernetesSecrets(this IConfigurationBuilder builder, string path = null, bool optional = true)
+        {
+            // Default path if not set.
+            path ??= "/etc/secrets";
+
+            // Default return if we cant find this folder to avoid runtime errors.  Worst case scenario
+            // is that the settings are not loaded from here.
+            if (!Directory.Exists(path))
+            {
+                return builder;
+            }
+
+            return builder.AddKeyPerFile(path, optional);
+        }
+
+        /// <summary>
+        /// Uses the desired default configurations.  Environment taken from EnvVariable "ENVIRONMENT" if not passed.
+        /// Builds configuration sources in the following order:
+        /// - 1. Kubernetes Secrets (looks in the "etc/secrets" folder by default)
+        /// - 2. Environment variables
+        /// - 3. Command line arguments
+        /// - 4. Json file (appsettings.json, followed by appsettings.{env}.json)
+        /// Note:
+        /// - appsettings.{env}.json WILL override appsettings.json file settings.
+        /// </summary>
+        /// <param name="builder">The configuration builder to bind to.</param>
+        /// <param name="appSettingsPath">The application settings path.</param>
+        /// <param name="environment">Specify the environment - optional, as its loaded from the ENVIRONMENT env variable if not set here.</param>
+        /// <param name="kubernetesSecretsPath">The K8S secrets path.</param>
+        /// <returns>The configuration builder after config has been added.</returns>
+        public static IConfigurationBuilder UseDefaultConfigs(this IConfigurationBuilder builder, string appSettingsPath = "appsettings.json", string kubernetesSecretsPath = null, string environment = null)
+        {
+            builder.AddKubernetesSecrets(kubernetesSecretsPath)
+                    .AddEnvironmentVariables()
+                    .AddCommandLine(Environment.GetCommandLineArgs())
+                    .AddJsonFile(appSettingsPath, true);
+
+            var env = Environment.GetEnvironmentVariable("ENVIRONMENT");
+
+            if (!string.IsNullOrEmpty(environment))
+                env = environment;
+
+            if (!string.IsNullOrEmpty(env))
+            {
+                builder.AddJsonFile($"appsettings.{env}.json", true, true);
+            }
+
+            return builder;
+        }
+
+        /// <summary>Adds the key vault secrets specified.  Uses Msi auth and gets the Key Vault url from "KEYVAULT_URL" setting.  If "KEYVAULT_URL" is not set, looks for "KeyVaultInstanceName" as backup.</summary>
+        /// <param name="builder">The builder to extend.</param>
+        /// <param name="params">The list of keys to load.</param>
+        /// <returns>IConfigurationBuilder with param keys as settings.</returns>
+        /// <exception cref="InvalidOperationException">Vault url must be set, ensure "KEYVAULT_URL" or "KeyVaultInstanceName" has been set in config</exception>
+        public static IConfigurationBuilder AddKeyVaultSecrets(this IConfigurationBuilder builder, params string[] @params)
+        {
+            // Get the expected keyvault url setting from the environment.
+            var vaultUrl = builder.GetValue<string>("KEYVAULT_URL");
+
+            if (string.IsNullOrEmpty(vaultUrl))
+            {
+                // If url was not set, look for an instance name and infer url.
+                var instanceName = builder.GetValue<string>("KeyVaultInstanceName");
+                vaultUrl = $"https://{instanceName}.vault.azure.net";
+            }
+
+            if (string.IsNullOrEmpty(vaultUrl))
+                throw new ArgumentException("Vault url must be set, ensure \"KEYVAULT_URL\" or \"KeyVaultInstanceName\" has been set in config", nameof(vaultUrl));
+
+            return AddKeyVaultSecrets(builder, new Uri(vaultUrl), @params.ToList());
+        }
+
+        /// <summary>
+        /// Adds the key vault secrets specified.  Uses Msi auth and builds the instance name on the fly.
+        /// Needs config value "KeyVaultInstanceName" to work.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="vaultUrl">Key vault url to connect to.</param>
+        /// <param name="keys">The list of keys to load.</param>
+        /// <param name="suppressKeyNotFoundError">If [true], when a key is missing an invalid operation exception will be thrown. If [false], the
+        /// error will be suppressed and it will just not add the key to the returned collection.</param>
+        /// <returns>IConfigurationBuilder.</returns>
+        /// <exception cref="InvalidOperationException">
+        /// Expecting setting "KeyVaultInstanceName" to infer instance name
+        /// or
+        /// Problem occurred retrieving secrets from KeyVault using Managed Identity
+        /// </exception>
+        public static IConfigurationBuilder AddKeyVaultSecrets(this IConfigurationBuilder builder, Uri vaultUrl, List<string> keys, bool suppressKeyNotFoundError = true)
+        {
+            try
+            {
+                if (vaultUrl == null)
+                    throw new ArgumentException("Vault url must be set", nameof(vaultUrl));
+
+                if (keys == default)
+                    return builder;
+
+                var vault = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(new AzureServiceTokenProvider().KeyVaultTokenCallback));
+                var secrets = new List<KeyValuePair<string, string>>();
+
+                // Gather secrets from Key Vault, one by one.
+                foreach (var key in keys)
+                {
+                    try
+                    {
+                        var secret = vault.GetSecretAsync(vaultUrl.AbsolutePath, key).GetAwaiter().GetResult();
+                        secrets.Add(new KeyValuePair<string, string>(key, secret.Value));
+                    }
+                    catch (KeyVaultErrorException e)
+                        when (e.Response.StatusCode == HttpStatusCode.NotFound && suppressKeyNotFoundError)
+                    {
+                        // Do nothing if it fails to find the value.
+                        Console.WriteLine($"Failed to find key vault setting: {key}, exception: {e.Message}");
+                    }
+                }
+
+                // Add them to config.
+                if (secrets.Any())
+                    builder.AddInMemoryCollection(secrets);
+
+                // Return updated builder.
+                return builder;
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Problem occurred retrieving secrets from KeyVault using Managed Identity", ex);
+            }
+        }
+
+        /// <summary>
+        /// Add key/value to config builder.
+        /// </summary>
+        /// <param name="builder">Builder to extend.</param>
+        /// <param name="key">Key for value being added.</param>
+        /// <param name="value">Value to add.</param>
+        /// <returns>Builder with key/value added.</returns>
+        public static IConfigurationBuilder AddValue(this IConfigurationBuilder builder, string key, string value)
+        {
+            return builder.AddInMemoryCollection(new List<KeyValuePair<string, string>> { new KeyValuePair<string, string>(key, value)});
+        }
+
+        /// <summary>
+        /// Add enumerable list of config values.
+        /// </summary>
+        /// <param name="builder">Builder to extend.</param>
+        /// <param name="values">List of values to add.</param>
+        /// <returns>Builder with values added.</returns>
+        public static IConfigurationBuilder AddValues(this IConfigurationBuilder builder, IEnumerable<KeyValuePair<string, string>> values)
+        {
+            return builder.AddInMemoryCollection(values);
+        }
+
+        /// <summary>
+        /// Extension to grab values from existing configs during the build process.
+        /// </summary>
+        /// <typeparam name="T">Type of config object being pulled.</typeparam>
+        /// <param name="builder">The builder being extended.</param>
+        /// <param name="key">The key for the config value to search for.</param>
+        /// <returns>T config value.</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static T GetValue<T>(this IConfigurationBuilder builder, string key)
+        {
+            if (builder == null)
+                throw new ArgumentException("Configuration builder must be set", nameof(builder));
+
+            return builder.Build().GetValue<T>(key);
+        }
+
+        /// <summary>
+        /// Gets value from config based on key.
+        /// </summary>
+        /// <param name="config">The configuration to get value from.</param>
+        /// <param name="key">The unique key which holds the wanted value.</param>
+        /// <param name="value">Out variable which returns found value if present.</param>
+        /// <returns>bool, true or false depending on if the value associated with the key could be found.</returns>
+        public static bool TryGetValue<T>(this IConfiguration config, string key, out T value)
+        {
+            key ??= "";
+
+            value = config.GetValue<T>(key);
+            if (value == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/Tests/Eshopworld.DevOps.Tests/ConfigBuilderExtensionTests.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Eshopworld.Tests.Core;
+using FluentAssertions;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+public class ConfigBuilderExtensionTests2 : IDisposable
+{
+    private const string TestAppSettingsFile = "appsettings_unit.json";
+    private const string TestKubernetestSecretFile = "TestKey2";
+    private readonly string _kubernetesTestSecretPath;
+
+    public ConfigBuilderExtensionTests2()
+    {
+        // Setup test files.
+        var currentDir = Directory.GetCurrentDirectory();
+        _kubernetesTestSecretPath = currentDir;
+
+        // Method 1 - app settings json file.
+        File.WriteAllText(Path.Combine(currentDir, TestAppSettingsFile), "{\"TestKey1\":\"testVal1\", \"TestKey2\": { \"TestKey3\":\"testVal3\" } }");
+
+        // Method 2 - Kubernetes Secrets simulation.
+        File.WriteAllText(Path.Combine(currentDir, TestKubernetestSecretFile), "testVal2");
+    }
+
+    /// <summary>Ensure BindBaseSection on the IConfigurationBuilder, binds root appsettings to a model as expected.</summary>
+    [Fact, IsUnit]
+    public void Test_ConfigBuilder_BindBaseSection()
+    {
+        // Arrange
+        var configBuilder = new ConfigurationBuilder();
+
+        // Act
+        configBuilder.UseDefaultConfigs(TestAppSettingsFile, _kubernetesTestSecretPath);
+        var boundConfig = configBuilder.Build().BindBaseSection<TestSettings>();
+
+        // Assert
+        boundConfig.TestKey1.Should().Be("testVal1");
+        boundConfig.TestKey2.Should().Be("testVal2");
+    }
+
+    /// <summary>Ensure AddKubernetesSecrets on the IConfigurationBuilder, adds config from Kubernetes secrets as expected.</summary>
+    [Fact, IsUnit]
+    public void Test_ConfigBuilder_AddKubernetesSecrets()
+    {
+        // Arrange
+        var configBuilder = new ConfigurationBuilder();
+        configBuilder.AddInMemoryCollection(new List<KeyValuePair<string, string>>
+        {
+            new KeyValuePair<string, string>("testKey", "testVal")
+        });
+
+        // Act
+        configBuilder.AddKubernetesSecrets(_kubernetesTestSecretPath);
+        var lookupResult = configBuilder.GetValue<string>("TestKey2");
+
+        // Assert
+        lookupResult.Should().NotBeNullOrEmpty();
+        lookupResult.Should().Be("testVal2");
+    }
+
+    /// <summary>Ensure using default configs loads all expected values from various sources (in the expected order).</summary>
+    [Fact, IsUnit]
+    public void Test_ConfigBuilder_UseDefaultConfigs()
+    {
+        // Arrange
+        IConfiguration configBuilder = new ConfigurationBuilder()
+            .UseDefaultConfigs(TestAppSettingsFile, Directory.GetCurrentDirectory())
+            .Build();
+
+        var settings = configBuilder.Get<TestSettings>();
+
+        // Act/Assert
+        settings.Should().NotBeNull();
+        settings.TestKey1.Should().NotBeNullOrEmpty(); // loaded from appsettings
+        settings.TestKey1.Should().Be("testVal1");
+        settings.TestKey2.Should().NotBeNullOrEmpty(); // loaded from kubernetes secret
+        settings.TestKey2.Should().Be("testVal2");
+    }
+
+    /// <summary>Ensure Use Default Builder with wrong paths still sets up the config with the right path.</summary>
+    [Fact, IsUnit]
+    public void Test_ConfigBuilder_UseDefaultConfigsWrongPaths()
+    {
+        // Arrange
+        IConfiguration configBuilder = new ConfigurationBuilder()
+            .UseDefaultConfigs("madeUpSettings.json", _kubernetesTestSecretPath)
+            .Build();
+
+        // Act
+        var settings = configBuilder.Get<TestSettings>();
+
+        // Assert
+        settings.Should().NotBeNull();
+        settings.TestKey1.Should().BeNullOrEmpty(); // loaded from appsettings
+        settings.TestKey2.Should().NotBeNullOrEmpty(); // loaded from kubernetes secret
+        settings.TestKey2.Should().Be("testVal2");
+    }
+
+    /// <summary>Ensure add value, adds a config setting as expected.</summary>
+    [Fact, IsUnit]
+    public void Test_ConfigBuilder_AddValue()
+    {
+        // Arrange
+        var configBuilder = new ConfigurationBuilder();
+        configBuilder.AddValue("testKey", "testVal");
+
+        // Act
+        var lookupResult = configBuilder.GetValue<string>("testKey");
+
+        // Assert
+        lookupResult.Should().Be("testVal");
+    }
+
+    /// <summary>Ensure add multiple values, adds as expected.</summary>
+    [Fact, IsUnit]
+    public void Test_ConfigBuilder_AddValues()
+    {
+        // Arrange
+        var configs = new List<KeyValuePair<string, string>> {
+            new KeyValuePair<string, string>("testKey", "testVal"),
+            new KeyValuePair<string, string>("testKey1", "testVal1"),
+            new KeyValuePair<string, string>("testKey2", "testVal2"),
+        };
+        var configBuilder = new ConfigurationBuilder();
+        configBuilder.AddValues(configs);
+
+        // Act
+        var lookupResult = configBuilder.GetValue<string>("testKey");
+        var lookupResult1 = configBuilder.GetValue<string>("testKey1");
+        var lookupResult2 = configBuilder.GetValue<string>("testKey2");
+
+        // Assert
+        lookupResult.Should().Be("testVal");
+        lookupResult1.Should().Be("testVal1");
+        lookupResult2.Should().Be("testVal2");
+    }
+
+    /// <summary>Ensure GetValue on the builder, returns the value as expected.</summary>
+    [Fact, IsUnit]
+    public void Test_ConfigBuilder_GetValue()
+    {
+        // Arrange
+        var configBuilder = new ConfigurationBuilder();
+        configBuilder.AddInMemoryCollection(new List<KeyValuePair<string, string>>
+        {
+            new KeyValuePair<string, string>("testKey", "testVal")
+        });
+
+        // Act
+        var lookupResult = configBuilder.GetValue<string>("testKey");
+
+        // Assert
+        lookupResult.Should().Be("testVal");
+    }
+
+    /// <summary>Ensure wrong keys don't return values.</summary>
+    [Fact, IsUnit]
+    public void Test_ConfigBuilder_TryGetValue_FailWithWrongKey()
+    {
+        // Arrange
+        IConfiguration configBuilder = new ConfigurationBuilder()
+            .UseDefaultConfigs(TestAppSettingsFile, Directory.GetCurrentDirectory())
+            .Build();
+
+        // Act/Assert
+        configBuilder.TryGetValue("WrongKey", out string value).Should().BeFalse();
+        value.Should().BeNullOrEmpty();
+    }
+
+    /// <summary>Ensure trying to get a value using a null key returns false for TryGet.</summary>
+    [Fact, IsUnit]
+    public void Test_ConfigBuilder_TryGetValue_FailWithNullKey()
+    {
+        // Arrange
+        IConfiguration configBuilder = new ConfigurationBuilder()
+            .UseDefaultConfigs(TestAppSettingsFile, Directory.GetCurrentDirectory())
+            .Build();
+
+        // Act/Assert
+        configBuilder.TryGetValue(null, out string value).Should().BeFalse();
+        value.Should().BeNullOrEmpty();
+    }
+
+    /// <summary>Ensure TryGet returns expected value and successful flag.</summary>
+    [Fact, IsUnit]
+    public void Test_ConfigBuilder_TryGetValue()
+    {
+        IConfiguration configBuilder = new ConfigurationBuilder()
+            .UseDefaultConfigs(TestAppSettingsFile, Directory.GetCurrentDirectory())
+            .Build();
+
+        // Act/Assert
+        configBuilder.TryGetValue("TestKey1", out string value).Should().BeTrue();
+        value.Should().NotBeNullOrEmpty();
+    }
+
+    /// <summary>Verify an argument exception occurs when "KEYVAULT_URL" and "KeyVaultInstanceName" is not set.</summary>
+    [Fact, IsUnit]
+    public void Test_KeyVault_Builder_AddKeyVaultSecretsWithParams_NoUrl()
+    {
+        // Arrange - Principle needs "Set" permissions to run this.
+        IConfigurationBuilder builder = new ConfigurationBuilder();
+
+        // Act
+        Action loadSettings = () => { builder.AddKeyVaultSecrets("key1", "key2"); };
+
+        // Assert
+        loadSettings.Should().Throw<ArgumentException>();
+    }
+
+    /// <summary>Verify invalid operation exception when wrong key vault is set.</summary>
+    [Fact, IsUnit]
+    public void Test_KeyVault_Builder_AddKeyVaultSecretsWithParams_InvalidKeyVault()
+    {
+        // Arrange - Principle needs "Set" permissions to run this.
+        IConfigurationBuilder builder = new ConfigurationBuilder();
+
+        // Act
+        builder.AddValue("KeyVaultInstanceName", "WrongKv");
+        Action loadSettings = () => { builder.AddKeyVaultSecrets("key1", "key2"); };
+
+        // Assert
+        loadSettings.Should().Throw<InvalidOperationException>();
+    }
+
+    /// <summary>Verify argument exception occurs when trying to add secrets but vault is not specified.</summary>
+    [Fact, IsUnit]
+    public void Test_KeyVault_Builder_AddKeyVaultSecrets_NoUri()
+    {
+        // Arrange - Principle needs "Set" permissions to run this.
+        IConfigurationBuilder builder = new ConfigurationBuilder();
+
+        // Act
+        Action loadSettings = () => { builder.AddKeyVaultSecrets(null, new List<string> {"key1", "key2"}); };
+
+        // Assert
+        loadSettings.Should().Throw<ArgumentException>();
+    }
+
+    /// <summary>
+    /// Verify key is not set but no error occurs when trying to add a key in key vault that does not exist.
+    /// PRESUMPTION: KEYVAULT_URL is set on the test server.
+    /// </summary>
+    [Fact, IsIntegration]
+    public void Test_KeyVault_Builder_AddKeyVaultSecrets_SecretNotAdded()
+    {
+        // Arrange - Principle needs "Set" permissions to run this.
+        IConfigurationBuilder builder = new ConfigurationBuilder();
+
+        // Act
+        builder.UseDefaultConfigs();
+        builder.AddKeyVaultSecrets("MadeUpKey1");
+        var config = builder.Build();
+
+        // Assert
+        config.TryGetValue<object>("MadeUpKey1", out _).Should().BeFalse();
+    }
+
+    /// <summary>
+    /// Verify a real key in keyvault is added as expected.
+    /// PRESUMPTION: KEYVAULT_URL is set on the test server.
+    /// </summary>
+    [Fact, IsIntegration]
+    public void Test_KeyVault_Builder_AddKeyVaultSecrets_SecretAdded()
+    {
+        // Arrange - Principle needs "Set" permissions to run this.
+        IConfigurationBuilder builder = new ConfigurationBuilder();
+
+        // Act
+        builder.UseDefaultConfigs();
+
+        // Set the test value in KV.
+        var vaultUrl = builder.GetValue<string>("KEYVAULT_URL");
+        var vault = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(new AzureServiceTokenProvider().KeyVaultTokenCallback));
+        vault.SetSecretAsync(vaultUrl, "RealKey1", "MyValue1").GetAwaiter().GetResult();
+
+        // Add the secret to the builder using extension method.
+        builder.AddKeyVaultSecrets("RealKey1");
+        var config = builder.Build();
+
+        // Assert
+        config.TryGetValue<object>("MadeUpKey1", out _).Should().BeFalse();
+    }
+
+    public void Dispose()
+    {
+        // Clean up test files.
+        var currentDir = Directory.GetCurrentDirectory();
+        File.Delete(Path.Combine(currentDir, TestAppSettingsFile));
+        File.Delete(Path.Combine(currentDir, TestKubernetestSecretFile));
+    }
+
+    private class TestSettings
+    {
+        public string TestKey1 { get; set; }
+        public string TestKey2 { get; set; }
+    }
+}
