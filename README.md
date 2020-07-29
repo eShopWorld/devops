d73707c3dd75bfafb799034b3d6692a7bb1313be
Rob McCabe <robert.mccabe@outlook.com>
2020-07-29 14:02:45 +0100

    Added KeyVault extensions (#31)
    * Added extension methods for adding individual key vault secrets and other useful configuration extensions
    
    * Updated devops package version
    
    * Fixed package version
    
    * Fixes following review
    
    * Made test async - fix
    
    * Fixed release notes
    
    * Fixed unit tests - added settings to existing AppSettings
    
    * Updated readme with explanation of problems connecting to key vault when using the extension method
    
    * Updated usage example
    
    * Updated readme notes
    
    * Updated readme
    
    * Removed kubernetes extensions from settings to narrow scope
    
    * Updated readme with realistic example
    
    * Updated readme with example
    
    * One last change (hopefully!) to the readme example
    
    * Changed Key Vault environment variable to constant
    
    * Use existing env variable key
    
    * Split tests into separate unit and integration test classes
    
    * Add unit test file that was missed
    
    * Update README.md
    
    * Fixed tests using namespaces
    
    * Updated following review
    
    * Fixed following review
    
    Co-authored-by: Robert McCabe <rmccabe@eshopworld.com>

--- README.md
+++ README.md
@@ -1,2 +1,196 @@
-# devops
-Eshopworld.DevOps package repo
+# Eshopworld.DevOps package
+
+[![Build status](https://dev.azure.com/eshopworld/Github%20build/_apis/build/status/devops)](https://dev.azure.com/eshopworld/Github%20build/_build/latest?definitionId=150)
+[![Coverage](https://sonarcloud.io/api/project_badges/measure?project=esw.devops&metric=coverage)](https://sonarcloud.io/dashboard?id=esw.devops)
+
+## Summary
+
+DevOps extensions, used for application configuration setup.
+
+## Load configurations
+
+```csharp
+public class Program
+{
+   ...
+   
+   
+    public void ConfigureAppConfiguration(IConfigurationBuilder builder)
+    {
+	// By calling the UseDefaultConfigs, settings are loaded from environment, command line args, followed by appsettings.
+        builder.UseDefaultConfigs();
+
+		...
+    }
+    ...
+   
+}
+```
+
+## Load individual secrets from a single Key Vault
+
+Individual secrets can be loaded into configuration from Key Vault in the following way:
+
+```csharp
+public class Program
+{
+   ...
+   
+    public void ConfigureAppConfiguration(IConfigurationBuilder builder)
+    {
+        builder.UseDefaultConfigs();
+
+        // Pass the name of the secrets you wish to load into configuration.
+        builder.AddKeyVaultSecrets(  
+			"TenantId", 
+			"SubscriptionId", 
+			"OtherSecretName");
+    }
+    ...
+   
+}
+```
+
+NOTE: When there's a problem pulling the "KEYVAULT_URL" config or the fallback "KeyVaultInstanceName" key (which the extension method `AddKeyVaultSecrets` uses), then an exception would be thrown to the calling code. This would (and should) happen during application bootstrap (either `wehHost.ConfigureAppConfiguration` or `genericHost.ConfigureAppConfiguration`). We want this because the app wont be able to load secrets it needs to run.
+
+## Load individual secrets from multiple key vaults
+
+Load from multiple Key Vaults in the following way:
+
+```csharp
+public class Program
+{
+   ...
+   
+    public void ConfigureAppConfiguration(IConfigurationBuilder builder)
+    {
+        builder.UseDefaultConfigs();
+
+		var kvInstance1 = new Uri("https://instance1.vault.azure.net");
+		var kvInstance2 = new Uri("https://instance2.vault.azure.net");
+
+        // Overload1 Add from key vault loaded in KEYVAULT_URL setting.
+        builder.AddKeyVaultSecrets("SomeKey1", "SomeKey2");
+
+        // Overload2 Pass the instance and list of the secrets you wish to load into configuration.
+        builder.AddKeyVaultSecrets(kvInstance1, new [] {
+			"TenantId", 
+			"SubscriptionId", 
+			"OtherSecretName" });
+		
+		builder.AddKeyVaultSecrets(kvInstance2, new [] {
+			"OtherKey1", 
+			"OtherKey2", 
+			"OtherKey3" });
+    }
+    ...
+   
+}
+
+```
+
+## Using the loaded configuration
+
+Take this class:
+
+```csharp
+public class AppSettings {
+	public string TenantId { get; set; }
+	public string SubscriptionId { get; set; }
+	public string OtherSecretName { get; set; }
+}
+```
+
+We can bind the settings to this class using the `BindBaseSection` call as follows:
+
+```csharp
+// Taken from Startup.cs after the ConfigureAppConfiguration above has been run.
+
+public void ConfigureServices(IServiceCollection services)
+{
+	// Example of binding settings directly to a class (without the "GetSection" call).
+	var appSettings = _configuration.BindBaseSection<AppSettings>();
+	
+	// Example of directly using the settings directly after they are loaded.
+	var tenantId = _configuration["TenantId"];
+	
+	var otherSecret = null;
+	
+	if (_configuration.TryGetValue<string>("OtherSecretName"), out otherSecret) 
+	{
+		... do something conditional if the setting exists ...
+	}
+}
+```
+
+## Example of using config with WebHostBuilder
+
+Example of using with a WebHostBuilder when bootstrapping a Web Application.
+
+```csharp
+public class Program
+{
+	...
+
+	// In program, we setup our configuration in the standard microsoft way...
+	private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+		WebHost.CreateDefaultBuilder(args)
+			.ConfigureAppConfiguration(config => {
+
+				// Import default configurations (env vars, command line args, appSettings.json etc).
+				config.UseDefaultConfigs();
+
+				// Load config from key vault.
+				config.AddKeyVaultSecrets(config.GetValue<string>("KeyVaultInstanceName"),
+					"TenantId",
+					"SubscriptionId",
+					"OtherSecretName");
+			})
+			.ConfigureLogging((context, logging) => {
+				
+				// Add logging configuration and loggers.
+				logging.AddConfiguration(context.Configuration)
+					.AddConsole()
+					.AddDebug();
+			})
+			.UseStartup<Startup>();
+
+	...
+}
+```
+
+```csharp
+public class Startup
+{
+	private readonly ILogger<Startup> _logger;
+	private readonly IConfiguration _configuration;
+
+	// We can then grab IConfiguration from the constructor, to use in our startup file as follows:
+	public Startup(IConfiguration configuration, ILogger<Startup> logger)
+	{
+		_configuration = configuration;
+		_logger = logger;
+	}
+
+	public void ConfigureServices(IServiceCollection services)
+	{
+		// You could bind directly to a poco class of your choice.
+		_appSettings = _configuration.BindBaseSection<AppSettings>();
+		
+		// Other setting bindings...
+		var bb = BigBrother.CreateDefault(_telemetrySettings.InstrumentationKey, _telemetrySettings.InternalKey);
+		_configuration.GetSection("Telemetry").Bind(_telemetrySettings);
+		_configuration.GetSection("HttpCors").Bind(_corsSettings);
+		_configuration.GetSection("RefreshingTokenProviderSettings").Bind(_refreshingTokenProviderOptions);
+		_configuration.GetSection("Endpoints").Bind(_endpoints);
+	}
+	
+	...
+}
+```
+
+## How to access this package
+All of the eshopworld.* packages are published to a public NuGet feed.  To consume this on your local development machine, please add the following feed to your feed sources in Visual Studio:
+https://eshopworld.myget.org/F/github-dev/api/v3/index.json
+ 
+For help setting up packages, follow this article: https://docs.microsoft.com/en-us/vsts/package/nuget/consume?view=vsts
---
+++ src/Eshopworld.DevOps/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,208 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.Configuration
+{
+    using Azure.KeyVault;
+    using Azure.KeyVault.Models;
+    using Azure.Services.AppAuthentication;
+    using Eshopworld.DevOps;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+
+    /// <summary>Class Configuration extensions.</summary>
+    public static class ConfigurationExtensions
+    {
+        /// <summary>
+        /// Binds the base section of the config to an actual class of type T.
+        /// </summary>
+        /// <param name="config">The configuration.</param>
+        /// <returns>IConfigurationSection.</returns>
+        public static T BindBaseSection<T>(this IConfiguration config)
+        {
+            if (config == null)
+                throw new ArgumentNullException( nameof(config), "Configuration must be set");
+
+            var configBase = new ConfigurationBuilder();
+            configBase.AddInMemoryCollection(config.GetChildren().Where(c => c.Value != null).Select(c => new KeyValuePair<string, string>("base:" + c.Key, c.Value)));
+            return configBase.Build().GetSection("base").Get<T>();
+        }
+
+        /// <summary>
+        /// Uses the desired default configurations.  Environment taken from EnvVariable "ENVIRONMENT" if not passed.
+        /// Builds configuration sources in the following order:
+        /// - 1. Environment variables
+        /// - 2. Command line arguments
+        /// - 3. Json file (appsettings.json, followed by appsettings.{env}.json)
+        /// Note:
+        /// - appsettings.{env}.json WILL override appsettings.json file settings.
+        /// </summary>
+        /// <param name="builder">The configuration builder to bind to.</param>
+        /// <param name="appSettingsPath">The application settings path.</param>
+        /// <param name="environment">Specify the environment - optional, as its loaded from the ENVIRONMENT env variable if not set here.</param>
+        /// <returns>The configuration builder after config has been added.</returns>
+        public static IConfigurationBuilder UseDefaultConfigs(this IConfigurationBuilder builder, string appSettingsPath = "appsettings.json", string environment = null)
+        {
+            builder.AddEnvironmentVariables()
+                    .AddCommandLine(Environment.GetCommandLineArgs())
+                    .AddJsonFile(appSettingsPath, true);
+
+            var env = EswDevOpsSdk.GetEnvironmentName();
+
+            if (!string.IsNullOrEmpty(environment))
+                env = environment;
+
+            if (!string.IsNullOrEmpty(env))
+            {
+                builder.AddJsonFile($"appsettings.{env}.json", true, true);
+            }
+
+            return builder;
+        }
+
+        /// <summary>Adds the key vault secrets specified.  Uses Msi auth and gets the Key Vault url from `EswDevOpsSdk.KeyVaultUrlKey` setting.  If url is not set, it falls back to "KeyVaultInstanceName" setting as backup.</summary>
+        /// <param name="builder">The builder to extend.</param>
+        /// <param name="params">The list of keys to load.</param>
+        /// <returns>IConfigurationBuilder with param keys as settings.</returns>
+        /// <exception cref="InvalidOperationException">Vault url must be set, ensure `EswDevOpsSdk.KeyVaultUrlKey` is set or "KeyVaultInstanceName" has been set in config</exception>
+        public static IConfigurationBuilder AddKeyVaultSecrets(this IConfigurationBuilder builder, params string[] @params)
+        {
+            // Get the expected key vault url setting from the environment.
+            var vaultUrl = builder.GetValue<string>(EswDevOpsSdk.KeyVaultUrlKey);
+
+            if (string.IsNullOrEmpty(vaultUrl))
+            {
+                // If url was not set, look for an instance name and infer url.
+                var instanceName = builder.GetValue<string>("KeyVaultInstanceName");
+                vaultUrl = $"https://{instanceName}.vault.azure.net";
+            }
+
+            // Verify the key vault url is set.
+            if (string.IsNullOrEmpty(vaultUrl))
+            {
+                throw new InvalidOperationException($"Vault url must be set, ensure \"{EswDevOpsSdk.KeyVaultUrlKey}\" or \"KeyVaultInstanceName\" have been set in config");
+            }
+
+            // Verify the key vault url is a valid url.
+            if (!(Uri.TryCreate(vaultUrl, UriKind.Absolute, out var kvUri)))
+            {
+                throw new InvalidOperationException($"Vault url \"{vaultUrl}\" is invalid");
+            }
+
+            return AddKeyVaultSecrets(builder, kvUri, @params);
+        }
+
+        /// <summary>
+        /// Adds the key vault secrets specified.  Uses Msi auth and builds the instance name on the fly.
+        /// Needs config value "KeyVaultInstanceName" to work.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="vaultUrl">Key vault url to connect to.</param>
+        /// <param name="keys">The list of keys to load.</param>
+        /// <param name="suppressKeyNotFoundError">If [true], when a key is missing an invalid operation exception will be thrown. If [false], the
+        /// error will be suppressed and it will just not add the key to the returned collection.</param>
+        /// <returns>IConfigurationBuilder.</returns>
+        /// <exception cref="ArgumentException">Vault url must be set</exception>
+        /// <exception cref="InvalidOperationException">Problem occurred retrieving secrets from KeyVault using Managed Identity</exception>
+        public static IConfigurationBuilder AddKeyVaultSecrets(this IConfigurationBuilder builder, Uri vaultUrl, IEnumerable<string> keys, bool suppressKeyNotFoundError = true)
+        {
+            if (vaultUrl == null)
+                throw new ArgumentNullException(nameof(vaultUrl), "Vault url must be set");
+
+            if (!keys.Any())
+                return builder;
+
+            try
+            {
+                var vault = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(new AzureServiceTokenProvider().KeyVaultTokenCallback));
+                var secrets = new List<KeyValuePair<string, string>>();
+
+                // Gather secrets from Key Vault, one by one.
+                foreach (var key in keys)
+                {
+                    try
+                    {
+                        var secret = vault.GetSecretAsync(vaultUrl.AbsolutePath, key).ConfigureAwait(false).GetAwaiter().GetResult();
+                        secrets.Add(new KeyValuePair<string, string>(key, secret.Value));
+                    }
+                    catch (KeyVaultErrorException e)
+                        when (e.Response.StatusCode == HttpStatusCode.NotFound && suppressKeyNotFoundError)
+                    {
+                        // Do nothing if it fails to find the value.
+                        Console.WriteLine($"Failed to find key vault setting: {key}, exception: {e.Message}");
+                    }
+                }
+
+                // Add them to config.
+                if (secrets.Any())
+                    builder.AddInMemoryCollection(secrets);
+
+                // Return updated builder.
+                return builder;
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Problem occurred retrieving secrets from KeyVault using Managed Identity", ex);
+            }
+        }
+
+        /// <summary>
+        /// Add key/value to config builder.
+        /// </summary>
+        /// <param name="builder">Builder to extend.</param>
+        /// <param name="key">Key for value being added.</param>
+        /// <param name="value">Value to add.</param>
+        /// <returns>Builder with key/value added.</returns>
+        public static IConfigurationBuilder AddValue(this IConfigurationBuilder builder, string key, string value)
+        {
+            return builder.AddInMemoryCollection(new List<KeyValuePair<string, string>> { new KeyValuePair<string, string>(key, value)});
+        }
+
+        /// <summary>
+        /// Add enumerable list of config values.
+        /// </summary>
+        /// <param name="builder">Builder to extend.</param>
+        /// <param name="values">List of values to add.</param>
+        /// <returns>Builder with values added.</returns>
+        public static IConfigurationBuilder AddValues(this IConfigurationBuilder builder, IDictionary<string, string> values)
+        {
+            return builder.AddInMemoryCollection(values);
+        }
+
+        /// <summary>
+        /// Extension to grab values from existing configs during the build process.
+        /// </summary>
+        /// <typeparam name="T">Type of config object being pulled.</typeparam>
+        /// <param name="builder">The builder being extended.</param>
+        /// <param name="key">The key for the config value to search for.</param>
+        /// <returns>T config value.</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static T GetValue<T>(this IConfigurationBuilder builder, string key)
+        {
+            if (builder == null)
+                throw new ArgumentException("Configuration builder must be set", nameof(builder));
+
+            return builder.Build().GetValue<T>(key);
+        }
+
+        /// <summary>
+        /// Gets value from config based on key.
+        /// </summary>
+        /// <param name="config">The configuration to get value from.</param>
+        /// <param name="key">The unique key which holds the wanted value.</param>
+        /// <param name="value">Out variable which returns found value if present.</param>
+        /// <returns>bool, true or false depending on if the value associated with the key could be found.</returns>
+        public static bool TryGetValue<T>(this IConfiguration config, string key, out T value)
+        {
+            key ??= "";
+
+            value = config.GetValue<T>(key);
+            if (value == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
