1bcfc30655f4ed7cbbab47984d9bc0a1fb97d716
Rob <rmccabe@eshopworld.com>
2020-09-03 10:11:11 +0100

    Added ability to use Dictionary for mapping without url

--- src/Tests/Eshopworld.DevOps.Tests/ConfigBuilderIntegrationTests.cs
+++ src/Tests/Eshopworld.DevOps.Tests/ConfigBuilderIntegrationTests.cs
@@ -109,8 +109,10 @@ public class ConfigBuilderIntegrationTests
 
         // Act
         builder.UseDefaultConfigs();
-        var url = builder.GetValue<string>("KEYVAULT_URL");
-        builder.AddKeyVaultSecrets(new Uri(url), new Dictionary<string, string> { { "keyVaultItem", "MappedName"} });
+        builder.AddKeyVaultSecrets(new Dictionary<string, string>
+        {
+            { "keyVaultItem", "MappedName"}
+        });
         var config = builder.Build();
 
         // Assert
