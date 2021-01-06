a7f3a57a518a5e08eac40a3cbaa80f0875fe75ed
Antonino Parisi <aparisi@eshopworld.com>
2021-01-06 12:15:27 +0000

    #102559 decorate the TelemetrySettings class with the KeyVaultSecretName attribute to allow for automagic secret binding

--- src/Eshopworld.DevOps/TelemetrySettings.cs
+++ src/Eshopworld.DevOps/TelemetrySettings.cs
@@ -1,18 +1,24 @@
-﻿namespace Eshopworld.DevOps
+﻿using System.Diagnostics.CodeAnalysis;
+using Eshopworld.DevOps.KeyVault;
+
+namespace Eshopworld.DevOps
 {
     /// <summary>
     /// Contains settings related to Telemetry.
     /// </summary>
+    [ExcludeFromCodeCoverage]
     public class TelemetrySettings
     {
         /// <summary>
         /// Gets and sets the main telemetry instrumentation key.
         /// </summary>
+        [KeyVaultSecretName("cm--ai-telemetry--instrumentation")]
         public string InstrumentationKey { get; set; }
 
         /// <summary>
         /// Gets and sets the internal instrumentation key.
         /// </summary>
+        [KeyVaultSecretName("cm--ai-telemetry--internal")]
         public string InternalKey { get; set; }
     }
 }
